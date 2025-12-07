bb9b35c16342afacebc1f6480edd2ac7ea9e9f12
Jobacke <johannes@vjbackhaus.com>
2025-12-07 11:28:44 +0100

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,57 @@
+// Service Worker für JUH Motorradgruppe Einsatzplanung
+const CACHE_NAME = 'juh-motorrad-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/icon-192.png',
+  '/icon-512.png'
+];
+
+// Installation
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Cache geöffnet');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Fetch - Network First Strategy (für Firebase)
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    fetch(event.request)
+      .then(response => {
+        // Wenn die Anfrage erfolgreich war, Response klonen und im Cache speichern
+        if (response && response.status === 200) {
+          const responseClone = response.clone();
+          caches.open(CACHE_NAME).then(cache => {
+            cache.put(event.request, responseClone);
+          });
+        }
+        return response;
+      })
+      .catch(() => {
+        // Bei Netzwerkfehler: Versuche aus dem Cache zu laden
+        return caches.match(event.request);
+      })
+  );
+});
+
+// Aktivierung - Alte Caches löschen
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            console.log('Lösche alten Cache:', cacheName);
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
